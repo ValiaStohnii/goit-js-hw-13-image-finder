19e96065581461fb7b459ed76d1290eaa8d9444e
ValiaStohnii <valia19961018@gmail.com>
2021-11-07 15:47:26 +0200

    add-load-btn

--- src/index.js
+++ src/index.js
@@ -1,31 +1,66 @@
 import './sass/main.scss';
 import ApiService from './js/apiService';
 import photoCardTpl from './partials/photo-card.hbs';
+import LoadButton from './js/load-button';
 
 const apiService = new ApiService();
+const loadButton = new LoadButton({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+});
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     galleryContainer: document.querySelector('.gallery'),
-    loadMoreButton: document.querySelector('.load-more__button'),
 }
 
 refs.searchForm.addEventListener('submit', onSearch);
-refs.loadMoreButton.addEventListener('click', onLoadMore);
+loadButton.refs.button.addEventListener('click', onLoadMore);
+
 
 function onSearch(e) {
     e.preventDefault();
 
+    loadButton.show();
+    clearGaleryContainer();
     apiService.query = e.currentTarget.elements.query.value;
+    window.addEventListener('keydown', onEnterKeyPress);
     apiService.resetPage();
-    apiService.fetchArticles().then(photosMarkup);
-
+    loadPhotos();
+    elementScrollIntoView();
 }
 
 function onLoadMore() {
-    apiService.fetchArticles().then(photosMarkup);
+    loadPhotos();
+    elementScrollIntoView();
+}
+
+function loadPhotos() {
+    loadButton.disable();
+    apiService.fetchArticles().then(photos => {
+        photosMarkup(photos);
+        loadButton.enable();
+    });
 }
 
 function photosMarkup(photos) {
     refs.galleryContainer.insertAdjacentHTML('beforeend', photoCardTpl(photos));
+}
+
+function clearGaleryContainer() {
+    refs.galleryContainer.innerHTML = '';
+}
+
+function onEnterKeyPress(event) {
+  if (event.code === 'Enter') {
+    onSearch();
+  }
+}
+
+function elementScrollIntoView () {
+    const element = document.getElementById('gallery_box');
+    element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+    });
 }
---
+++ src/js/load-button.js
@@ -0,0 +1,32 @@
+export default class LoadButton{
+    constructor({ selector, hidden = false }) {
+        this.refs = this.getRefs(selector);
+        hidden && this.hide();
+    }
+
+    getRefs(selector) {
+        const refs = {};
+        refs.button = document.querySelector(selector);
+        refs.label = refs.button.querySelector('.label');
+
+        return refs;
+    }
+
+    hide() {
+        this.refs.button.classList.add('is-hidden');
+    }
+
+    show() {
+        this.refs.button.classList.remove('is-hidden');
+    }
+
+    enable() {
+        this.refs.button.disabled = false;
+        this.refs.label.textContent = 'Load more';
+    }
+
+    disable() {
+        this.refs.button.disabled = true;
+        this.refs.label.textContent = 'Load...';
+    }
+}
